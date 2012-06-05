ccb51699c1c1454857487f694cd84bee1eeb3fb4
amine chouki <surfeux@gmail.com>
2012-06-05 11:00:34 +0000

    mofication des templates tags

--- socialregistration/contrib/foursquare/templatetags/foursquare.py
+++ socialregistration/contrib/foursquare/templatetags/foursquare.py
@@ -1,6 +1,21 @@
+from classytags.core import Tag, Options
+from classytags.arguments import Argument, KeywordArgument
+from classytags.helpers import InclusionTag
 from django import template
-from socialregistration.templatetags import button
+from profile.models import Profile, LEVELS
 
 register = template.Library()
 
-register.tag('foursquare_button', button('socialregistration/foursquare/foursquare_button.html'))
+class FoursquareTag(Tag):
+    name = "foursquare_button"
+    
+    options = Options(Argument('template_id', required=False, resolve=False))
+    
+    def render_tag(self, context, template_id=None):
+        if template_id:
+             self.template = 'socialregistration/foursquare/foursquare_button_%s.html' % template_id
+        else:
+            self.template = 'socialregistration/foursquare/foursquare_button.html'
+        return template.loader.render_to_string(self.template, context)
+    
+register.tag(FoursquareTag)
